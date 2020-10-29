2da26b6e3c971c084bd07bc9296c24276e4d2923
ANDY <h880702520@gmail.com>
2020-10-30 03:00:58 +0800

    Update Carousel.vue

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -5,19 +5,20 @@
         <v-icon 
             large class="switchBtn" 
             id="prev" 
-            @click="subtractCounter() ; Slide()">
+            @click="Counter(-1) ; Slide()">
             fa-angle-left
         </v-icon>
         <v-icon 
             large class="switchBtn" 
             id="next" 
-            @click="addCounter() ; Slide()">
+            @click="Counter(1) ; Slide()">
             fa-angle-right
         </v-icon>
         <!-- 輪播圖片 -->
         <div class="switch_photo d-flex ">
-            <v-card v-for="(item, index) in products" :key="index" tile max-width="400" >
-                <v-img :src="item.url" max-width="400" max-height="400" class="my-auto"></v-img>
+            <v-card v-for="(item, index) in products" :key="index" tile max-width="355">
+                <v-img :src="item.url" max-width="355" max-height="355" class="my-auto"></v-img>
+                <v-card-subtitle class="text-center heading-6">{{ item.title }}</v-card-subtitle>
             </v-card>
         </div>
     </div>
@@ -33,22 +34,23 @@ export default {
         }
     },
     methods: {
-        addCounter () {
-            this.counter++
+        // 增加 或 減少 counter 計數
+        Counter (value) {
+            this.counter += value
             console.log(this.counter);
         },
-        subtractCounter () {
-            this.counter--
-            console.log(this.counter);
-        },
-        // 輪播圖片每次移動 -400px
+        // 依據 counter 輪播圖片每次移動 (-355 x counter) px, 355px 為 v-card 寬度
         Slide () {
+            // 先取得使用者當前螢幕的寬度
+            let clientWidth = document.querySelector('.carousel').offsetWidth
+            
+            // 
             const slide = document.querySelector('.switch_photo')
-            slide.style.transform = `translateX( ${-(this.counter) * 400}px )`
+            slide.style.transform = `translateX( ${-(this.counter) * 355}px )`
             slide.style.transition = "transform 0.4s ease-in-out"
             // 若圖片已達最左邊
             if (this.counter < 0) {
-                // 將移動量重為 0 (即不動)
+                // 將移動量重設為 0 (即不動)
                 slide.style.transform = `translateX(0)`
                 this.counter = 0
             }
@@ -79,4 +81,12 @@ export default {
         color: red;
         cursor: pointer;
     }
+    .v-card__subtitle {
+        background: rgba(0, 0, 0, 0.7);
+        color:white !important;
+    }
+
+    // @media (max-width: 412px) {
+    //     
+    // }
 </style>
