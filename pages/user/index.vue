211688062377555c23e7005108e787efc94caff1
ANDY <h880702520@gmail.com>
2021-03-31 17:20:12 +0800

    add user's page and store/user.js

---
+++ pages/user/index.vue
@@ -0,0 +1,29 @@
+<template>
+<v-container>
+    <div>歡迎您 {{ user.email }}</div>
+</v-container>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+    computed: {
+        ...mapGetters({
+            user: 'user/getUserInfo'
+        })
+    },
+    methods: {
+        ...mapActions({
+            fetchUserInfo: 'user/fetchUserInfo'
+        })
+    },
+    mounted() {
+        this.fetchUserInfo()
+    }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
