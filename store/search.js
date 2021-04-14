1291b2060bec9c08481ca41c45ba92a27061a813
ANDY <h880702520@gmail.com>
2021-04-14 23:59:13 +0800

    add condition of accepting empty string

--- store/search.js
+++ store/search.js
@@ -21,19 +21,24 @@ export const actions = {
     // 搜尋功能
     async searchProducts({ commit }, title) {
         try {
-            const res = await apiSearchByTitle(title)
-            // 將結果寫入 state
-            let result = res.data.products
-            await commit('SET_SEARCH_PRODUCTS', result)
-            // 導向至搜尋結果
-            this.$router.push({
-                name: 'search-title',
-                params: { title: title },
-            })
+            // 若有輸入字符
+            if (title) {
+                const res = await apiSearchByTitle(title)
+                // 將結果寫入 state
+                let result = res.data.products
+                await commit('SET_SEARCH_PRODUCTS', result)
+                // 導向至搜尋結果
+                this.$router.push({
+                    name: 'search-title',
+                    params: { title: title },
+                })
+            } else {
+                // 導向至首頁
+                this.$router.push({ name: 'index' })
+            }
         } catch (error) {
-            console.log(error);
-            let msg = '搜尋失敗'
-            alert(msg)
+            console.log(error)
+            console.log("搜尋失敗 from /store/search.js");
         }
     },
 }
