24e8245c558f3ff79837f7a6350504e831445e64
ANDY <h880702520@gmail.com>
2021-04-21 01:45:24 +0800

    add pending status in store

--- store/pagination.js
+++ store/pagination.js
@@ -2,27 +2,38 @@ import { apiGetProductsWithPagination } from '../APIs/api.js'
 
 export const state = () => ({
     products: [],
+    pending: false
 })
 
 export const getters = {
     getPaginatedProducts(state) {
         return state.products
     },
+    getPending(state) {
+        return state.pending
+    }
 }
 
 export const mutations = {
     SET_PAGINATED_PRODUCTS(state, products) {
         state.products = products
     },
+    SET_PENDING(state, pending) {
+        state.pending = pending
+    }
 }
 
 export const actions = {
     async fetchPaginatedProducts({ commit }, pageNumber) {
         try {
+            // pending 狀態
+            commit('SET_PENDING', true)
+            // 撈取資料
             const res = await apiGetProductsWithPagination(pageNumber)
             let products = res.data.products
             await commit('SET_PAGINATED_PRODUCTS', products)
-            // 導向至該頁
+            commit('SET_PENDING', false)
+            // 最後導向至該頁
             this.$router.push({
                 name: 'pagination-pageNumber',
                 params: { pageNumber: pageNumber },
