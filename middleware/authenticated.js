98ca6402e8b6b7e71ebb5db618b0dc1389d8614c
ANDY <h880702520@gmail.com>
2021-04-22 00:21:53 +0800

    Update authenticated.js

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -2,7 +2,5 @@ export default async function({ store, redirect }) {
     // // 未驗證導向登入頁面
     if (!store.state.auth.user) {
         return redirect({ name: 'auth-login' })
-    } else {
-        return redirect({ name: 'index' })
     }
 }
