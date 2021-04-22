e93263715ea53f7681bb0191525bcd37f305e4ac
ANDY <h880702520@gmail.com>
2021-04-22 20:40:11 +0800

    fix comments

--- APIs/api.js
+++ APIs/api.js
@@ -1,36 +1,36 @@
 import axios from 'axios';
 axios.defaults.withCredentials = true;
 
-// URL 與 設定
-let base = 'http://localhost:8000/api'; // 後端 API 的 URL
+//* URL 與 設定
+let base = 'http://localhost:8000/api'; //? 後端 API 的 URL
 
 
-// CSRF請求------------------------------------------------------------------Start
+//* CSRF請求------------------------------------------------------------------Start
 const userCsrfRequest = axios.create({
     baseURL: 'http://localhost:8000'
 });
-// CSRF請求------------------------------------------------------------------End
+//* CSRF請求------------------------------------------------------------------End
 
 
-// 產品請求-------------------------------------------------------------------Start
+//* 產品請求-------------------------------------------------------------------Start
 const productRequest = axios.create({
     baseURL: `${ base }/v1/products`
 });
-// 產品請求-------------------------------------------------------------------End
+//* 產品請求-------------------------------------------------------------------End
 
 
-// 使用者請求------------------------------------------------------------------Start
+//* 使用者請求------------------------------------------------------------------Start
 const userRequest = axios.create({
     baseURL: `${ base }/v1/user`
 });
-// 使用者請求------------------------------------------------------------------End
+//* 使用者請求------------------------------------------------------------------End
     
 
-// 驗證請求-------------------------------------------------------------------Start
+//* 驗證請求-------------------------------------------------------------------Start
 const authRequest = axios.create({
     baseURL: `${ base }/v1/auth/user`
 });
-// 設置攔截器 (interceptors)
+//? 設置攔截器 (interceptors)
 authRequest.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem('Token')
@@ -41,40 +41,40 @@ authRequest.interceptors.request.use(
     }, 
     (error) => Promise.reject(error)
 )
-// 驗證請求-------------------------------------------------------------------End
+//* 驗證請求-------------------------------------------------------------------End
 
 
 
 //------------------------------------------- 使用者相關 API -------------------------------------------//
-    // 註冊 
+    //* 註冊 
 export const apiUserRegister = data => userRequest.post('/register', data);
-    // 登入 (取得 Token)
+    //* 登入 (取得 Token)
 export const apiUserLogin = data => userRequest.post('/login', data);
-    // CSRF 
+    //* CSRF 
 export const apiCsrfLogin = () => userCsrfRequest.get('/sanctum/csrf-cookie');
     //----------------------------------- 以下操作必須包含Token -------------------------------------//
-    // 登出 
+    //* 登出 
 export const apiUserLogout = () => authRequest.get('/logout');
-    // 取得使用者 (暫時未用到)
+    //* 取得使用者 (暫時未用到)
 export const apiGetUserInfo = () => authRequest.get('/getUser');
 //------------------------------------------- 使用者相關 API -------------------------------------------//
 
 
 
 //------------------------------------------- 商品相關的 API -------------------------------------------//
-    // 撈取所有產品
+    //* 撈取所有產品
 export const apiGetProducts = () => productRequest.get('/');
-    // 網站首頁的產品
+    //* 網站首頁的產品
 export const apiGetIndexPageProducts = () => productRequest.get('/indexPage');
-    // 圖片輪播產品
+    //* 圖片輪播產品
 export const apiGetCarouselProducts = () => productRequest.get('/carousel');
-    // 撈取單一產品
+    //* 撈取單一產品
 export const apiGetProduct = (id) => productRequest.get(`/${id}`);
-    // 商品換頁(Pagination) 
+    //* 商品換頁(Pagination) 
 export const apiGetProductsWithPagination = (page) => productRequest.get(`/pagination?page=${page}`);
-    // 搜尋商品(名稱)
+    //* 搜尋商品(名稱)
 export const apiSearchByTitle = (title) => productRequest.get(`/search/${title}`);
-    // 搜尋商品(標籤)  暫時未用到
+    //! 搜尋商品(標籤)  暫時未用到
 export const apiSearchByTag = (id) => productRequest.get(`/tag/${id}`);
 //------------------------------------------- 商品相關的 API -------------------------------------------//
 
@@ -82,43 +82,43 @@ export const apiSearchByTag = (id) => productRequest.get(`/tag/${id}`);
 
 //-------------------------------------------------- 購物車相關 API --------------------------------------------------//
             //----------------------------------- 以下操作必須包含Token -------------------------------------//
-// 讀取
-    // 撈取購物車中的產品
+//* 讀取
+    //? 撈取購物車中的產品
 export const apiGetCartProducts = () => authRequest.get('/cart');
-// 新增
-    // 新增商品至購物車
+//* 新增
+    //? 新增商品至購物車
 export const apiAddToCart = (productId) => authRequest.get(`/cart/${productId}/create`);
-    // 新增商品至購物車(包含使用者所輸入的數量)
+    //? 新增商品至購物車(包含使用者所輸入的數量)
 export const apiAddToCartWithQuantity = (productId, quantity) => authRequest.post(`/cart/${productId}/create`, quantity);
-// 刪除
-    // 購物車中移除商品
+//* 刪除
+    //? 購物車中移除商品
 export const apiDeleteFromCart = (productId) => authRequest.delete(`/cart/${productId}/delete`);
-    // 清空購物車
+    //? 清空購物車
 export const apiDeleteAllFromCart = () => authRequest.delete(`/cart/deleteAll`);
-// 修改
-    // 直接輸入
+//* 修改
+    //? 直接輸入
 export const apiUpdateQuantity = (productId, quantity) => authRequest.post(`/cart/${productId}/update`, quantity);
-    // 增加 1
+    //? 增加 1
 export const apiIncreseQuantityByOne = (productId) => authRequest.get(`/cart/${productId}/increseByOne`);
-    // 減少 1
+    //? 減少 1
 export const apiDecreseQuantityByOne = (productId) => authRequest.get(`/cart/${productId}/decreseByOne`);
 //-------------------------------------------------- 購物車相關 API --------------------------------------------------//
 
 
 //-------------------------------------------------- 訂單相關 API --------------------------------------------------//
             //----------------------------------- 以下操作必須包含Token -------------------------------------//
-// 讀取
-    // 後端傳來的表單資料
+//* 讀取
+    //? 後端傳來的表單資料
     export const apiGetFormData = () => authRequest.get('/order/getFormData');
-    // 所有訂單
+    //? 所有訂單
     export const apiGetAllOrders = () => authRequest.get('/order');
-    // 單筆訂單
+    //? 單筆訂單
     export const apiGetSingleOrder = (orderId) => authRequest.get(`/order/${orderId}`);
-// 新增
-    // 建立訂單
+//* 新增
+    //? 建立訂單
     export const apiCreateOrder = (data) => authRequest.post('/order/create', data);
-// 刪除
-    // 刪除訂單
+//* 刪除
+    //? 刪除訂單
     export const apiDeleteSingleOrder = (orderId) => authRequest.delete(`/order/${orderId}/delete`);
 //-------------------------------------------------- 訂單相關 API --------------------------------------------------//
     
