ec1ecb17b18e1a9c73d05ef64350ae0b38b5e32e
ANDY <h880702520@gmail.com>
2021-04-24 17:04:20 +0800

    Create EmptyOrder.vue

---
+++ components/order/EmptyOrder.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="text-center">
+        <!-- 圖片 -->
+        <v-img :src="require('~/static/order/NoDataLeft.svg')"
+            class="mx-auto mb-4" 
+            max-width="300"
+            max-height="300">
+        </v-img>
+        <!-- 文字 -->
+        <h2 class="my-4">您目前沒有任何訂單。</h2>
+        <!-- 導向連結 -->
+        <nuxt-link  :to="{ name: 'pagination-pageNumber', params: { pageNumber: 1 }}">
+            <v-btn color="primary" x-large depressed>
+                <v-icon>fa-cart-arrow-down</v-icon>
+                <span class="mx-2">點我選購~</span> 
+            </v-btn>
+        </nuxt-link>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
