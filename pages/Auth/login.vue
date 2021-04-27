aba4f6d92131c97ce2a4743a0e3468c675ea6462
ANDY <h880702520@gmail.com>
2021-04-27 10:30:27 +0800

    refactor auth page with mixins

--- pages/Auth/login.vue
+++ pages/Auth/login.vue
@@ -87,47 +87,20 @@
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
+import { mapActions } from 'vuex'
+import authMixin from '~/mixins/authMixin'
 
 export default {
-    data() {
-        return {
-            //* 表單中的值
-            form: {
-                email: '',
-                password: '',
-            },
-            //* 表單驗證規則
-            valid: null, //* 是否合格
-            show: false, //* 顯示 or 不顯示 密碼
-            rules: {
-                required: value => !!value || '此欄位必填',
-                min: value => value.length >= 6 || '至少需要6個英文或數字',
-                email: value => {
-                    const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-                    return pattern.test(value) || '範例 : abc123@gmail.com'
-                },
-            },
-        }
-    },
+    mixins: [authMixin],
     methods: {
         ...mapActions({
             //* 登入使用者
             login: 'auth/login',
-            //* 確認使用者是否已經登入
-            checkIfUserHasLoggedIn: 'auth/checkIfUserHasLoggedIn'
-        }),
-    },
-    computed: {
-        ...mapGetters({
-            //* 錯誤訊息
-            message: 'auth/getMessage',
-            //* loading 狀態
-            loading: 'auth/getLoading'
         }),
     },
     mounted() {
-       this.checkIfUserHasLoggedIn()
+        //* 此處的 function 在 authMixin 裡面
+       this.checkIfUserHasLoggedIn() //? 確認使用者是否已經登入
     }
 }
 </script>
