57327469c662cf70d792b0d7f0da3d1f42e4399e
ANDY <h880702520@gmail.com>
2021-05-04 16:17:38 +0800

    Create orderMixin.js

---
+++ mixins/orderMixin.js
@@ -0,0 +1,22 @@
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+    computed: {
+        ...mapGetters({
+            //* 使用者的資料
+            user: 'auth/getUserInfo',
+            //* 訂單相關的資料
+            orders: 'order/getAllOrders', //* 所有訂單
+            payments: 'order/getPayments', //* 付款方式
+            status: 'order/getStatus', //* 商品狀態
+            //* loading 狀態
+            loading: 'order/getLoading'
+        }),
+    },
+    methods: {
+        ...mapActions({
+            //* 刪除訂單
+            deleteSingleOrder: 'order/deleteSingleOrder',
+        })
+    }
+}
