f5016c3b0e9b2dc9ec54fe0fdcbd3e5176343063
ANDY <h880702520@gmail.com>
2021-05-05 21:35:57 +0800

    create Notification component

---
+++ components/notification/EmptyNotification.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="text-center">
+        <!-- 圖片 -->
+        <v-img
+            :src="require('~/static/notification/EmptyNotification.svg')"
+            class="mx-auto mb-4"
+            max-width="300"
+            max-height="300"
+        >
+        </v-img>
+        <!-- 提示文字 -->
+        <h2 class="mb-4">目前沒有任何通知。</h2>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
