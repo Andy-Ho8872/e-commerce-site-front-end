3382ff32bceab62a3f09d2e95b01b5078fc48902
ANDY <h880702520@gmail.com>
2021-05-07 00:40:46 +0800

    Create user.vue

---
+++ pages/user.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-container>
+        <!-- loading -->
+        <LoadingCircle v-if="!user"/>
+        <div v-else>
+            <UserTabs />
+            <nuxt-child />
+        </div>
+    </v-container>
+</template>
+
+<script>
+import userMixin from '~/mixins/userMixin'
+
+export default {
+    mixins: [userMixin]
+}
+</script>
+
+<style lang="scss"scoped>
+@media (max-width: 768px) {
+    .btn_container {
+        text-align: center;
+    }
+}
+</style>
