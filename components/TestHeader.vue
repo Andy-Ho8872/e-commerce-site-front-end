33a4288865d28b0ad460f14c84886261566b1eff
ANDY <h880702520@gmail.com>
2021-05-14 10:11:29 +0800

    Create TestHeader.vue

---
+++ components/TestHeader.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-container>
+        <v-tabs>
+            <v-tab nuxt :to="{ name: 'index' }">
+                <v-icon>fa-home fa-fw</v-icon>
+                <span>首頁</span>
+            </v-tab>
+            <v-tab nuxt :to="{ name: 'user-notification' }">
+                <v-icon>fa-bell fa-fw</v-icon>
+                <span>通知</span>
+            </v-tab>
+            <v-tab nuxt :to="{ name: 'user-order' }">
+                <v-icon>fa-list-alt fa-fw</v-icon>
+                <span>訂單</span>
+            </v-tab>
+            <v-tab nuxt :to="{ name: 'user-account' }">
+                <v-icon>fa-user fa-fw</v-icon>
+                <span>會員</span>
+            </v-tab>
+        </v-tabs>
+    </v-container>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            tab: null
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
