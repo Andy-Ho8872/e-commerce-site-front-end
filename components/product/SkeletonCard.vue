98dba6775d754ff3ef8cf774d64c9a770fdf0acf
ANDY <h880702520@gmail.com>
2021-05-18 15:51:13 +0800

    add margin auto

--- components/product/SkeletonCard.vue
+++ components/product/SkeletonCard.vue
@@ -1,22 +1,14 @@
 <template>
 <!-- 骨架屏 -->
-    <v-sheet tile elevation="2" class="pb-2" :width="cardWidth">
+    <v-sheet tile elevation="2" class="pb-2 mx-auto" :width="cardWidth">
         <!-- 圖片和標題 -->
-        <v-skeleton-loader 
-            type="card">
-        </v-skeleton-loader>
+        <v-skeleton-loader type="card"></v-skeleton-loader>
         <!-- 文字敘述 -->
-        <v-skeleton-loader 
-            type="list-item-two-line">
-        </v-skeleton-loader>
+        <v-skeleton-loader type="list-item-two-line"></v-skeleton-loader>
         <!-- 標籤 -->
         <div class="d-flex">
-            <v-skeleton-loader class="mx-4"
-                type="chip">
-            </v-skeleton-loader>
-            <v-skeleton-loader
-                type="chip">
-            </v-skeleton-loader>
+            <v-skeleton-loader class="mx-4" type="chip"></v-skeleton-loader>
+            <v-skeleton-loader type="chip"></v-skeleton-loader>
         </div>
     </v-sheet>
 </template>
