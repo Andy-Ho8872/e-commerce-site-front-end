f04150d684bfc90924478db65b6dcd6adee2b098
ANDY <h880702520@gmail.com>
2021-05-21 01:02:18 +0800

    Update axios.js

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,8 +1,42 @@
-export default function ({ $axios, redirect }) {
-    $axios.onError(error => {
-      if (error.response.status === 404) {
-        // redirect('/sorry')
-        alert('Http 請求發生錯誤: 來自 plugins/axios.js')
-      }
+export default function({ $axios, redirect }) {
+    $axios.onRequest(config => {
+        config.headers.authorization = localStorage.getItem('Token')
     })
-  }
+    $axios.onError(error => {
+        const code = parseInt(error.response && error.response.status)
+        switch (code) {
+            case 401:
+                console.log('沒權限')
+                redirect({ name: 'auth-login' })
+                break
+            case 500:
+                console.log('Server 錯誤')
+            default:
+                redirect({ name: 'auth-register' })
+                break
+        }
+    })
+    //-------------------------------------------------- 購物車相關 API --------------------------------------------------//
+                //----------------------------------- 以下操作必須包含Token -------------------------------------//
+    //* 讀取
+        //? 撈取購物車中的產品
+    $axios.apiGetCartProducts = () => $axios.get('/cart');
+    //* 新增
+        //? 新增商品至購物車
+    $axios.apiAddToCart = (productId) => $axios.get(`/cart/${productId}/create`);
+        //? 新增商品至購物車(包含使用者所輸入的數量)
+    $axios.apiAddToCartWithQuantity = (productId, quantity) => $axios.post(`/cart/${productId}/create`, quantity);
+    //* 刪除
+        //? 購物車中移除商品
+    $axios.apiDeleteFromCart = (productId) => $axios.delete(`/cart/${productId}/delete`);
+        //? 清空購物車
+    $axios.apiDeleteAllFromCart = () => $axios.delete(`/cart/deleteAll`);
+    //* 修改
+        //? 直接輸入
+    $axios.apiUpdateQuantity = (productId, quantity) => $axios.post(`/cart/${productId}/update`, quantity);
+        //? 增加 1
+    $axios.apiIncreseQuantityByOne = (productId) => $axios.get(`/cart/${productId}/increseByOne`);
+        //? 減少 1
+    $axios.apiDecreseQuantityByOne = (productId) => $axios.get(`/cart/${productId}/decreseByOne`);
+    //-------------------------------------------------- 購物車相關 API --------------------------------------------------//
+}
