5b98c84aac716af73a1f129db6c3ba8e8fcc398d
ANDY <h880702520@gmail.com>
2021-06-18 00:32:29 +0800

    Create Banner.vue

---
+++ components/index/Banner.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-card class="my-6 d-flex" :color="background">
+        <v-img
+            :src="require('~/static/index/Fire.svg')"
+            max-width="24"
+            max-height="24"
+            class="my-auto mx-2"
+        ></v-img>
+        <v-card-title>{{ text }}</v-card-title>
+    </v-card>
+</template>
+
+<script>
+export default {
+    props: {
+        'text': {
+            type: String,
+            require: true
+        },
+        'background': {
+            type: String,
+        }
+    },
+}
+</script>
