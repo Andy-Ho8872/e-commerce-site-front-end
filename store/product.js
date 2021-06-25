d397b3c30cdb3a71afa24a2ed73b8f02c7add356
ANDY <h880702520@gmail.com>
2021-06-25 11:39:21 +0800

    add state, mutation, action for indexPageProducts

--- store/product.js
+++ store/product.js
@@ -1,16 +1,21 @@
-//! 目前此頁面暫不使用
-import { apiGetProducts, apiGetProduct, apiGetProductTags } from '~/APIs/api.js'
+import { apiGetProducts, apiGetProduct, apiGetProductTags, apiGetIndexPageProducts } from '~/APIs/api.js'
 
 export const state = () => ({
-    //* 所有產品
+    //* 首頁的商品
+    indexPageProducts: [],
+    //* 瀏覽過的商品(暫存用)
     products: [],
-    //* 單一產品(由 id 進行篩選)
-    product: {}, // 起始為空的 Object
+    // 單一產品(由 id 進行篩選) //! 暫時不用
+    product: {},
     //* 產品標籤
-    productTags:[]
+    productTags: [],
 })
 
 export const getters = {
+    getIndexPageProducts(state) {
+        return state.indexPageProducts
+    },
+    //* 瀏覽過的商品(暫存用)
     getAllProducts(state) {
         return state.products
     },
@@ -19,34 +24,48 @@ export const getters = {
         return state.product
     },
     getAllProductTags(state) {
-        return state.tags
-    }
+        return state.productTags
+    },
 }
 
 export const mutations = {
-    //* 所有商品
+    SET_INDEX_PAGE_PRODUCTS(state, products) {
+        state.indexPageProducts = products
+    },
+    // 所有瀏覽過的商品 //! 暫時不用
     SET_PRODUCTS(state, products) {
         state.products = products
     },
     //* 單一商品
     SET_PRODUCT(state, product) {
         state.product = product
-        console.log(state.product)
     },
-    //* 將單一產品推送至所有產品(作為快取用) 
+    //* 紀錄瀏覽過的產品(作為快取用)
     PUSH_PRODUCT(state, product) {
         state.products.push(product)
     },
     //! 重設單品 (清空原有的 Object) 暫時未用
     RESET_PRODUCT(state) {
         state.product = {}
-    },  
+    },
     SET_PRODUCT_TAGS(state, tags) {
         state.productTags = tags
-    }
+    },
 }
 
 export const actions = {
+    async fetchIndexPageProducts({ state, commit }) {
+        //* state 中沒有資料才撈取 
+        if(!state.indexPageProducts.length) {
+            try {
+                const res = await apiGetIndexPageProducts()
+                const products = res.data.products
+                commit('SET_INDEX_PAGE_PRODUCTS', products)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    },
     //! 抓取所有商品 (暫時未用)
     async fetchAllProducts(state) {
         try {
@@ -59,28 +78,32 @@ export const actions = {
         }
     },
     //* 抓取單一商品
-    async fetchSingleProduct({ state, commit }, productId) { 
+    async fetchSingleProduct({ state, commit }, productId) {
         let exist = state.products.find(product => product.id == productId)
-        //* 若是找不到才抓取 
-        if(exist === undefined) {
+        //* 若是找不到才抓取
+        if (exist === undefined) {
             try {
                 const res = await apiGetProduct(productId)
                 //* 所有產品的資料
                 const product = res.data.product
+                //* 將點擊過的產品放入 products 暫存，若使用者在瀏覽過相同產品的時候就不用再發送一次 request
                 await commit('PUSH_PRODUCT', product)
-                commit('SET_PRODUCT', product)
+                //! 暫時不用 commit('SET_PRODUCT', product)
             } catch (error) {
                 console.log(error)
             }
         }
     },
-    async fetchAllProductTags({ commit }) {
-        try {
-            const res = await apiGetProductTags()
-            const tags = res.data.tags
-            commit('SET_PRODUCT_TAGS', tags)
-        } catch (error) {
-            console.log(error);
+    async fetchAllProductTags({ state, commit }) {
+        //* state 裡面沒有資料才撈取
+        if (!state.productTags.length) {
+            try {
+                const res = await apiGetProductTags()
+                const tags = res.data.tags
+                commit('SET_PRODUCT_TAGS', tags)
+            } catch (error) {
+                console.log(error)
+            }
         }
-    }
+    },
 }
