d08845f9fde63feeb922d9c6d34e45553720dc27
ANDY <h880702520@gmail.com>
2021-07-10 01:03:01 +0800

    Update error.vue

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,34 +1,24 @@
 <template>
-    <v-app dark class="text-center">
-        <v-sheet
-            color="blue-grey darken-4"
-            class="background d-flex justify-start align-center flex-column"
-        >
-        <!-- page not found -->
-        <div v-if="error.statusCode === 404" class="d-flex justify-start align-center flex-column">
-            <v-img
-                :src="require('~/static/error/404.svg')"
-                class="responsive_img"
-                max-width="600"
-                max-height="400"
-            ></v-img>
-            <h1 class="background_title white--text font-italic">{{ pageNotFound }}</h1>
+    <v-app dark class="text-center">  
+        <div class="full_screen d-flex justify-start align-center flex-column">
+            <!-- background img -->
+            <v-img :src="require('~/static/error/Background.svg')" class="full_screen">
+                <!-- 404 not found -->
+                <div v-if="error.statusCode === 404">
+                    <v-img :src="require('~/static/error/404.svg')" class="full_screen mx-auto" max-width="600" max-height="400"></v-img>
+                    <h1 class="error_message white--text font-italic text-center">{{ pageNotFound }}</h1>
+                </div>
+                <!-- other error -->
+                <div v-else>
+                    <v-img :src="require('~/static/error/BugFixing.svg')" class="full_screen mx-auto" max-width="600" max-height="400"></v-img>
+                    <h1 class="error_message white--text font-italic text-center">{{ otherError }}</h1>
+                </div>
+                <!-- action button -->
+                <div class="btn_container text-center mx-auto">
+                    <v-btn x-large color="warning" rounded nuxt :to="{ name: 'index' }">回到首頁</v-btn>
+                </div>
+            </v-img>
         </div>
-        <!-- other error -->
-        <div v-else class="d-flex justify-start align-center flex-column">
-            <v-img
-                :src="require('~/static/error/BugFixing.svg')"
-                class="responsive_img"
-                max-width="600"
-                max-height="400"
-            ></v-img>
-            <h1 class="background_title white--text font-italic">{{ otherError }}</h1>
-        </div>
-        <!-- 導覽按鈕 -->
-        <div class="btn_container text-center">
-            <v-btn x-large color="warning" rounded nuxt :to="{ name: 'index' }">回到首頁</v-btn>
-        </div>
-        </v-sheet>
     </v-app>
 </template>
 
@@ -58,29 +48,28 @@ export default {
 </script>
 
 <style scoped>
-.background {
-    height: 100%;
+.full_screen {
+    width: 100vw;
+    height: 100vh;
 }
-.background_title {
+.error_message {
     font-family: 'Poppins', sans-serif;
     font-size: 6rem;
     transform: translateY(-40%);
     letter-spacing: 8px;
 }
-.responsive_img {
-    width: 100%;
-}
 .btn_container {
     width: 120px;
 }
 @media (max-width: 768px) {
-    .background_title {
+    .error_message {
         font-size: 3rem;
     }
 }
 @media (max-width: 414px) {
-    .background_title {
+    .error_message {
         font-size: 1.75rem;
+        transform: translateY(-2.5em);
     }
 }
 </style>
