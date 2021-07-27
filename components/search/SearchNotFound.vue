4df3afa9f126ca08d0c6a81b2e7f26b4455474fb
Andy-Ho8872 <h880702520@gmail.com>
2021-07-27 17:12:53 +0800

    Create SearchNotFound.vue

---
+++ components/search/SearchNotFound.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h2 class="ma-auto">
+            找不到關於
+            <span class="blue--text lighten-5">
+                "{{ searchQuery }}"
+            </span>
+            的搜尋結果
+        </h2>
+        <v-img :src="require('~/static/search/SearchNotFound.svg')"
+            class="mx-auto mb-4"
+            :max-width="width" 
+            :max-height="height"
+        >
+        </v-img>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        width: {
+            type: [String, Number],
+            default: 300
+        },
+        height: {
+            type: [String, Number],
+            default: 300
+        },
+        searchQuery: {
+            type: String,
+            default: '找不到相關的結果'
+        }
+    },
+}
+</script>
