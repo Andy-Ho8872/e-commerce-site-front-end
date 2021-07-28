729c505ad9a9512a627b66c57e8782edb1aed1c6
Andy-Ho8872 <h880702520@gmail.com>
2021-07-28 18:06:55 +0800

    refactor api search function as pagination version

--- store/search.js
+++ store/search.js
@@ -1,6 +1,12 @@
-import { apiSearchByTitle, apiSearchAutoComplete } from '../APIs/api.js'
+import {
+    // apiSearchByTitle, //! 舊版本 暫不使用
+    apiSearchAutoComplete,
+    apiSearchByTitleWithPagination,
+} from '../APIs/api.js'
 
 export const state = () => ({
+    //* 搜尋關鍵字
+    searchText: '',
     //* 搜尋結果
     searchResult: [],
     //* loading 狀態
@@ -11,6 +17,9 @@ export const state = () => ({
 })
 
 export const getters = {
+    getSearchText(state) {
+        return state.searchText
+    },
     getResult(state) {
         return state.searchResult
     },
@@ -26,6 +35,9 @@ export const getters = {
 }
 
 export const mutations = {
+    SET_SEARCH_TEXT(state, text) {
+        state.searchText = text
+    },
     SET_SEARCH_RESULT(state, result) {
         state.searchResult = result
     },
@@ -62,7 +74,7 @@ export const actions = {
         }
         commit('SET_AUTO_COMPLETE_LOADING', false)
     },
-    //* 搜尋功能
+    // 搜尋功能(舊版本) //! 暫時不使用
     async searchProducts({ commit }, title) {
         commit('SET_PAGE_LOADING', true)
         try {
@@ -77,6 +89,47 @@ export const actions = {
                     name: 'search-title',
                     params: { title: title },
                 })
+            } else {
+                //* 導向至首頁
+                this.$router.push({ name: 'index' })
+            }
+        } catch (error) {
+            console.log(error)
+            console.log('搜尋失敗 from /store/search.js')
+        }
+        commit('SET_PAGE_LOADING', false)
+    },
+    //* 搜尋功能(含分頁)
+    async searchProductsWithPagination({ commit }, payload) {
+        commit('SET_PAGE_LOADING', true)
+        //* 使用者輸入的關鍵字
+        commit('SET_SEARCH_TEXT', payload.title)
+        try {
+            //* 若有輸入字符才向後端發送請求
+            if (payload.title) {
+                const res = await apiSearchByTitleWithPagination(
+                    payload.title,
+                    payload.pageNumber
+                )
+                //* 將結果寫入 state
+                const result = res.data.products
+                await commit('SET_SEARCH_RESULT', result)
+                //* 若有搜尋結果
+                if (result) {
+                    //* 導向至搜尋結果
+                    this.$router.push({
+                        name: 'search-title-pagination-pageNumber',
+                        params: {
+                            title: payload.title,
+                            pageNumber: payload.pageNumber,
+                        },
+                    })
+                } else {
+                    //* 導向至搜尋失敗畫面
+                    this.$router.push({
+                        name: 'search-searchNotFound',
+                    })
+                }
             } else {
                 //* 導向至首頁
                 this.$router.push({ name: 'index' })
