eda1a9d695d9ad784402b939f50c8ae28aa7e355
Andy-Ho8872 <h880702520@gmail.com>
2021-07-28 18:07:04 +0800

    Create searchNotFound.vue

---
+++ pages/search/searchNotFound.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-container>
+        <div class="text-center">
+            <SearchNotFound :searchQuery="searchQuery"/>
+        </div>
+    </v-container>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+    computed: {
+        ...mapGetters({
+            searchQuery: 'search/getSearchText'
+        })
+    }
+}
+</script>
