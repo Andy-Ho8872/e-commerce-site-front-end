f5d2bd2335078fa9ba9865292b4c5d69ddfe6446
Andy-Ho8872 <h880702520@gmail.com>
2021-08-17 22:45:29 +0800

    add router test and initSetup function to set component status

--- tests/header/header.spec.js
+++ tests/header/header.spec.js
@@ -5,7 +5,7 @@ import Vuex from 'vuex'
 // components
 import Header from '@/components/header/Header.vue'
 // Utilities
-import { createLocalVue, RouterLinkStub, shallowMount } from '@vue/test-utils'
+import { createLocalVue, RouterLinkStub, mount } from '@vue/test-utils'
 // vuex modules
 import auth from '@/store/__mocks__/auth.js'
 import cart from '@/store/__mocks__/cart.js'
@@ -17,22 +17,27 @@ localVue.use(Vuex)
 localVue.use(VueRouter)
 
 describe('Header.vue', () => {
-    let router
-    let store
-    beforeEach(() => {
-        router = new VueRouter()
-        store = new Vuex.Store({
-            modules: {
-                auth,
-                cart,
-                notification
-            },
-        })
+    // presets
+    const routes = [
+        {
+            path: '/auth/login',
+            name: 'auth-login',
+        },
+    ]
+    const vuetify = new Vuetify()
+    const router = new VueRouter({ routes })
+    const store = new Vuex.Store({
+        modules: {
+            auth,
+            cart,
+            notification,
+        },
     })
-
-    test('vuex actions should be called once', async () => {
-        const wrapper = shallowMount(Header, {
+    // 初始化元件的狀態
+    const initSetup = () => {
+        return mount(Header, {
             localVue,
+            vuetify,
             store,
             router,
             stubs: {
@@ -41,6 +46,10 @@ describe('Header.vue', () => {
                 AutoComplete: true, // fake component
             },
         })
+    }
+    // tests
+    test('vuex actions should be called once', async () => {
+        const wrapper = initSetup()
         // 找出有 logout function 的DOM
         const logoutBtn = wrapper.findAll('li')
         // logout function
@@ -49,5 +58,16 @@ describe('Header.vue', () => {
         await logoutBtn.trigger('click')
         // 觸發完成
         expect(logout).toBeCalled()
+    })
+    test('login route should be pushed correctly after triggering a click event', async () => {
+        const wrapper = initSetup()
+        // 找出有切換路由功能的 DOM
+        const loginButton = wrapper.findAll('li')
+        // 等待點擊事件觸發
+        await loginButton.trigger('click')
+        // 觸發後切換路由
+        router.push({ name: 'auth-login' })
+        // APP 網址應該包含該路由的路徑
+        expect(wrapper.vm.$route.path).toContain('auth/login')
     })
 })
