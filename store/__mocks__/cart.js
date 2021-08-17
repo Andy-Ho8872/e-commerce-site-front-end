3ec7efc111a9526cdd676d9ea22f8d7d573511d6
Andy-Ho8872 <h880702520@gmail.com>
2021-08-18 00:07:04 +0800

    update mock data

--- store/__mocks__/cart.js
+++ store/__mocks__/cart.js
@@ -11,20 +11,42 @@ export const state = () => ({
             quantity: 1,
         },
     ],
+    product: {
+        id: 1,
+        title: 'test1',
+        quantity: 1,
+    },
+    loading: false
 })
 
 export const getters = {
     getUserCart(state) {
         return state.userCart
     },
+    getLoading(state) {
+        return state.loading
+    },
+    getProduct(state) {
+        return state.product
+    }
 }
 
 export const mutations = {
-    
+    INCREMENT(state) {
+        state.product.quantity += 1
+    },
+    DECREMENT(state) {
+        state.product.quantity -= 1
+    },
 }
 
 export const actions = {
-    
+    async increaseByOne({ commit }) {
+        commit('INCREMENT')
+    },
+    async decreaseByOne({ commit }) {
+        commit('DECREMENT')
+    },
 }
 
 export default {
