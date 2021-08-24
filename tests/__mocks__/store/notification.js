9c1b41dc2833927e994dd6121d853bd870778727
Andy-Ho8872 <h880702520@gmail.com>
2021-08-25 01:09:16 +0800

    update unReadNotification's default data

--- tests/__mocks__/store/notification.js
+++ tests/__mocks__/store/notification.js
@@ -2,11 +2,23 @@ export const state = () => ({
     unReadNotifications: [
         {
             id: 1,
-            text: 'fake notification-1',
+            data: [
+                {
+                    title: 'fake notification-1',
+                    order_id: 5,
+                    created_at: '2021/08/25',
+                },
+            ],
         },
         {
             id: 2,
-            text: 'fake notification-2',
+            data: [
+                {
+                    title: 'fake notification-2',
+                    order_id: 6,
+                    created_at: '2021/08/26',
+                },
+            ],
         },
     ],
 })
@@ -17,18 +29,14 @@ export const getters = {
     },
 }
 
-export const mutations = {
-    
-}
+export const mutations = {}
 
-export const actions = {
-    
-}
+export const actions = {}
 
 export default {
     namespaced: true,
     state,
     getters,
     mutations,
-    actions
+    actions,
 }
