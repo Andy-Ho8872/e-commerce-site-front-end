f22c19adbb326cc052e5b1dd86bcbed1a1d2ad01
Andy-Ho8872 <h880702520@gmail.com>
2021-08-26 16:53:18 +0800

    add mock store

---
+++ tests/__mocks__/store/order.js
@@ -0,0 +1,55 @@
+export const state = () => ({
+    //* 單筆訂單 (測試)
+    order: {
+        id: 1,
+        items: [
+            {
+                id: 1,
+                title: "item-1"
+            },
+            {
+                id: 2,
+                title: "item-22"
+            },
+        ]
+    },
+    //* 所有訂單
+    orders: [],
+    //* 付款方式
+    payments: [],
+    //* 商品狀態
+    status: [],
+    //* */ loading 狀態
+    loading: false
+})
+export const getters = {
+    getAllOrders(state) {
+        return state.orders
+    },
+    getLoading(state) {
+        return state.loading
+    },
+    getPayments(state) {
+        return state.payments
+    },
+    getStatus(state) {
+        return state.status
+    }
+}
+export const mutations = {
+    //* 訂單相關
+    SET_ALL_ORDERS(state, orders) {
+        state.orders = orders
+    },
+}
+export const actions = {
+    fetchAllOrders: jest.fn(),
+    fetchTableColumns: jest.fn()
+}
+export default {
+    namespaced: true,
+    state,
+    getters,
+    mutations,
+    actions
+}
