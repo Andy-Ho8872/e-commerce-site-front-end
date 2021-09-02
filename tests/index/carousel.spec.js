c47b92f8073ed50ae1b3466222390e29f448e9db
Andy-Ho8872 <h880702520@gmail.com>
2021-09-02 18:09:19 +0800

    delete unused module

--- tests/index/carousel.spec.js
+++ tests/index/carousel.spec.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
-// import Vuex from 'vuex'
 import VueRouter from 'vue-router'
 // components
 import CarouselV3 from '@/components/index/CarouselV3.vue'
