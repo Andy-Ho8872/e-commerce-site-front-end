d7a9e5346f262be1048dca28b50e36209b95c900
Andy-Ho8872 <h880702520@gmail.com>
2021-09-04 19:35:38 +0800

    add "add to cart" and "remove from cart" tests

--- tests/pages/cart/index.spec.js
+++ tests/pages/cart/index.spec.js
@@ -82,11 +82,10 @@ describe('cart.vue', () => {
             router,
             store,
         })
-        const discountedPrice = wrapper.findAll('.unit_price > .original_price')
-        expect(discountedPrice.at(0).classes()).toContain('discounted') // discount_rate 0.85
-        expect(discountedPrice.at(1).classes()).not.toContain('discounted') // discount_rate 1 (should not contain "discounted" class)
+        const originalPrice = wrapper.findAll('.unit_price > .original_price')
+        expect(originalPrice.at(0).classes()).toContain('discounted') // discount_rate 0.85
+        expect(originalPrice.at(1).classes()).not.toContain('discounted') // discount_rate 1 (should not contain "discounted" class)
     })
-
     test('should render discounted price when discounted_rate is not equal to 1', async () => {
         const wrapper = mount(cartPage, {
             stubs: {
@@ -102,5 +101,100 @@ describe('cart.vue', () => {
         })
         const discountedPrice = wrapper.findAll('.unit_price > .discounted_price')
         expect(discountedPrice.at(0).exists()).toBeTruthy() // discount_rate 0.85
+    })
+    test('should render subtotal correctly', async () => {
+        const wrapper = mount(cartPage, {
+            stubs: {
+                NuxtLink: RouterLinkStub, // fake router
+                EmptyCart: true, // fake component
+                Banner: true, // fake component
+                QuantityField: true, // fake component
+            },
+            localVue,
+            vuetify,
+            router,
+            store,
+        })
+        const subtotal = wrapper.find('.subtotal > span')
+        // item 1    850
+        // item 2    500
+        // subtotal = item 1 + item 2
+        expect(subtotal.text()).toBe('1350')
+    })
+    test('deleteFromCart action should be call with REMOVE_SINGLE_PRODUCT_FROM_CART mutation', async () => {
+        const wrapper = mount(cartPage, {
+            stubs: {
+                NuxtLink: RouterLinkStub, // fake router
+                EmptyCart: true, // fake component
+                Banner: true, // fake component
+                QuantityField: true, // fake component
+            },
+            localVue,
+            vuetify,
+            router,
+            store,
+        })
+        const productId = 1
+        const commit = jest.fn()
+        await cart.actions.deleteFromCart({ commit }, productId)
+        expect(commit).toBeCalledWith('REMOVE_SINGLE_PRODUCT_FROM_CART', productId)
+    })
+    test('should delete item with product_id by clicking delete single item button', async () => {
+        const wrapper = mount(cartPage, {
+            stubs: {
+                NuxtLink: RouterLinkStub, // fake router
+                EmptyCart: true, // fake component
+                Banner: true, // fake component
+                QuantityField: true, // fake component
+            },
+            localVue,
+            vuetify,
+            router,
+            store,
+        })
+        const userCart = store.state.cart.userCart
+        const valid = store.state.cart.valid
+        const deleteBtn = wrapper.findAll('.delete_single_item')
+        await deleteBtn.at(0).trigger('click') // 點擊第一個刪除按鈕
+        expect(userCart).toHaveLength(1) // 原本有兩筆資料，但刪除了一筆
+        // 因為前一筆商品(單眼相機)被刪除了，所以連身長裙的 index 從 1 變為 0
+        expect(userCart[0].title).toBe('連身長裙') // product_id = 2
+        // 購物車內還有商品存在，所以 valid 狀態為 true
+        expect(valid).toBeTruthy()
+    })
+    test('deleteAllFromCart action should be call with REMOVE_ALL_FROM_CART mutation', async () => {
+        const wrapper = mount(cartPage, {
+            stubs: {
+                NuxtLink: RouterLinkStub, // fake router
+                EmptyCart: true, // fake component
+                Banner: true, // fake component
+                QuantityField: true, // fake component
+            },
+            localVue,
+            vuetify,
+            router,
+            store,
+        })
+        const commit = jest.fn()
+        await cart.actions.deleteAllFromCart({ commit })
+        expect(commit).toBeCalledWith('REMOVE_ALL_FROM_CART')
+    })
+    test('should clear all items by clicking delete all items button', async () => {
+        const wrapper = mount(cartPage, {
+            stubs: {
+                NuxtLink: RouterLinkStub, // fake router
+                EmptyCart: true, // fake component
+                Banner: true, // fake component
+                QuantityField: true, // fake component
+            },
+            localVue,
+            vuetify,
+            router,
+            store,
+        })
+        const userCart = store.state.cart.userCart // fake data
+        const deleteAllBtn = wrapper.find('.delete_all_items')
+        await deleteAllBtn.trigger('click')
+        expect(userCart).toHaveLength(0)
     })
 })
