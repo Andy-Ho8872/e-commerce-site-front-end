011805902bff330d1bbacd3dcfb1300fedebdd8f
Andy-Ho8872 <h880702520@gmail.com>
2021-09-06 10:16:19 +0800

    Update create.spec.js

--- tests/pages/order/create.spec.js
+++ tests/pages/order/create.spec.js
@@ -9,6 +9,18 @@ import order from '@/tests/__mocks__/store/order.js'
 import cart from '@/tests/__mocks__/store/cart.js'
 // Utilities
 import { createLocalVue, RouterLinkStub, mount } from '@vue/test-utils'
+// mock api calls and data
+import { mockApiCreateOrder } from '@/tests/__mocks__/APIs/api.js'
+import mockAxios from 'axios'
+jest.mock('axios')
+mockAxios.post.mockResolvedValue({
+    data: {
+        order: {
+            payment_id: 1,
+            address: '桃園市OO區OO路'
+        }
+    },
+})
 // use packages or library
 Vue.use(Vuetify)
 const localVue = createLocalVue()
@@ -122,5 +134,29 @@ describe('order/index.vue', () => {
         })
         const createOrderBtn = wrapper.find('.create_order_btn')
         expect(createOrderBtn.props('disable')).toBeFalsy()
+    })
+    test('create order button should call api', async () => {
+        const wrapper = mount(orderPage, {
+            stubs: {
+                NuxtLink: RouterLinkStub, // fake router
+            },
+            localVue,
+            vuetify,
+            router,
+            store,
+        })
+        // 表單通過驗證
+        await wrapper.setData({
+            valid: true,
+        })
+        const createOrderBtn = wrapper.find('.create_order_btn')
+        await createOrderBtn.trigger('click')
+        const result = await mockApiCreateOrder()
+        expect(result.order.payment_id).toBe(1)
+        expect(result.order.address).toBe('桃園市OO區OO路')
+        // test vuex action
+        const commit = jest.fn()
+        await order.actions.createOrder({ commit }, result.order)
+        expect(commit).toBeCalledWith('SET_LOADING', true)
     })
 })
