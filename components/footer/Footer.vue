42fa3c7f12c4fc382e72d3ce115286e22abe0ce6
Andy-Ho8872 <h880702520@gmail.com>
2021-09-12 18:45:31 +0800

    update social links

--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -10,16 +10,16 @@
             <h2>社群連結</h2>
             <div class="links_container d-md-flex">
                 <li>
-                    <a href="https://github.com/Andy-Ho8872"><i class="fab fa-github"></i></a>
+                    <a href="https://github.com/Andy-Ho8872" target="_blank"><i class="fab fa-github"></i></a>
                 </li>
                 <li>
-                    <a href="https://twitter.com/home?lang=zh-tw"><i class="fab fa-twitter"></i></a>
+                    <a href="https://twitter.com/home?lang=zh-tw" target="_blank"><i class="fab fa-twitter"></i></a>
                 </li>
                 <li>
-                    <a href="https://www.facebook.com/"><i class="fab fa-facebook"></i></a>
+                    <a href="https://www.facebook.com/profile.php?id=100002704069788" target="_blank"><i class="fab fa-facebook"></i></a>
                 </li>
                 <li>
-                    <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+                    <a href="https://www.instagram.com/andy_ho8872/" target="_blank"><i class="fab fa-instagram"></i></a>
                 </li>
             </div>
         </ul>
@@ -61,6 +61,14 @@ export default {}
 <style lang="scss" scoped>
 $blue-gray-text-lighten-3: #b0bec5;
 $blue-gray-text-lighten-4: #cfd8dc;
+// bg color
+$social_link_bg_color: #37474f; // darken-3
+$social_link_hover_bg_color: #455A64; // darken-2
+// icon color
+$social_link_hover_github_icon_color: #FAFAFA;
+$social_link_hover_twitter_icon_color: #1DA1F2;
+$social_link_hover_facebook_icon_color: #4267B2;
+$social_link_hover_instagram_icon_color: #E1306C;
 li {
     list-style: none;
     text-decoration: none;
@@ -74,6 +82,7 @@ h2 {
     color: $blue-gray-text-lighten-4;
 }
 a {
+    //nuxt-link
     text-decoration: none;
     color: $blue-gray-text-lighten-3;
     &:hover {
@@ -91,22 +100,39 @@ a {
         display: flex;
         justify-content: center;
         align-items: center;
+        background: $social_link_bg_color;
         width: 40px;
         height: 40px;
-        background: #37474f;
         text-decoration: none;
         color: inherit;
         cursor: pointer;
         font-size: 1.5em;
         border-radius: 4px;
-        &:hover {
-            background: #fb8c00;
-            transition: background ease-in 0.2s;
-        }
     }
     .links_container {
         li {
             margin-right: 12px;
+            // icon background
+            & a:hover {
+                background: $social_link_hover_bg_color;
+            }
+            // icon color
+            &:nth-child(1) a:hover {
+                color: $social_link_hover_github_icon_color; 
+                text-shadow: 0 0 16px $social_link_hover_github_icon_color;
+            }
+            &:nth-child(2) a:hover {
+                color: $social_link_hover_twitter_icon_color;
+                text-shadow: 0 0 16px $social_link_hover_twitter_icon_color;
+            }
+            &:nth-child(3) a:hover {
+                color: $social_link_hover_facebook_icon_color;
+                text-shadow: 0 0 16px $social_link_hover_facebook_icon_color;
+            }
+            &:nth-child(4) a:hover {
+                color: $social_link_hover_instagram_icon_color;
+                text-shadow: 0 0 16px $social_link_hover_instagram_icon_color;
+            }
         }
     }
 }
