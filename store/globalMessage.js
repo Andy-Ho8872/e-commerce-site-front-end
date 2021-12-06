9bec8a740f51c2ec7cf49881cb9a77c5aaac8a3f
AndyHo <h880702520@gmail.com>
2021-12-06 15:45:05 +0800

    add clearTimeout to fix memory leak problem

--- store/globalMessage.js
+++ store/globalMessage.js
@@ -22,8 +22,10 @@ export const actions = {
         //* 設置訊息 
         commit('SET_MESSAGE', message)
         //* 3秒後清除訊息 
-        setTimeout(() => {
+        const timeout = setTimeout(() => {
             commit('CLEAR_MESSAGE')
+            //* 清除 timeout 以防止記憶體洩漏 
+            clearTimeout(timeout)
         }, 3000)
     }
 }
