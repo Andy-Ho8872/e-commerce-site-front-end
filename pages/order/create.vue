53737dd3aafe064b111ffbdeb4537c0d0b4ba4fd
AndyHo <h880702520@gmail.com>
2021-12-10 18:35:16 +0800

    add new input fields in create order page

--- pages/order/create.vue
+++ pages/order/create.vue
@@ -37,30 +37,42 @@
         </table>
         <!-- 表單 -->
         <v-form ref="form" v-model="valid">
-            <v-card class="checkout_warpper my-12 ma-auto" elevation="4">
+            <v-card class="checkout_wrapper my-12 ma-auto" elevation="4">
                 <!-- 選擇付款方式 -->
-                    <v-card-title>選擇您的付款方式</v-card-title>
+                    <v-card-title class="font-weight-bold">選擇您的付款方式</v-card-title>
                     <v-card-subtitle class="mt-2">
-                        <!-- <label class="ma-2 blue blue-grey lighten-4" v-for="payment in payments" :key="payment.id">
-                            
-                            <input type="radio" name="payment_id" 
-                                v-model="form.payment_id"
-                                :value="payment.id" 
-                                :checked="payment.id == 1"
-                            >
-                            <div>
-                                <v-icon dark>fa-bookmark fa-fw</v-icon>
-                                <span>{{ payment.title }}</span>
-                            </div>
-                        </label> -->
                         <v-chip-group mandatory active-class="deep-purple--text text--accent-4" name="payment_id" v-model="form.payment_id">
-                            <v-chip v-for="payment in payments" :key="'payment' + payment.id" :value="payment.id" class="payment_title">
+                            <v-chip v-for="payment in payments" :key="'payment' + payment.id" :value="payment.id" large label class="payment_title">
                                 {{ payment.title }}
                             </v-chip>
                         </v-chip-group>
                     </v-card-subtitle>
-                <!-- 選擇送達地址 -->
-                    <v-card-title>輸入您的地址</v-card-title>
+                <!-- 輸入訂單資訊 -->
+                    <v-card-title class="font-weight-bold">輸入訂單詳細資料</v-card-title>
+                    <!-- 姓名 -->
+                    <v-text-field
+                        class="text_field"
+                        name="name"
+                        v-model="form.name"
+                        :rules="[rules.required, rules.lettersOnly]"
+                        outlined
+                        label="您的姓名"
+                        placeholder="王小明"
+                        hide-details="auto"
+                    ></v-text-field>
+                    <!-- 電話 -->
+                    <v-text-field
+                        class="text_field"
+                        name="phone"
+                        v-model="form.phone"
+                        :rules="[rules.required, rules.numbersOnly]"
+                        maxlength="10"
+                        outlined
+                        label="您的電話"
+                        placeholder="0912345678"
+                        hide-details="auto"
+                    ></v-text-field>
+                    <!-- 地址 -->
                     <v-text-field
                         class="text_field"
                         name="address"
@@ -71,6 +83,23 @@
                         placeholder="OO市OO區OO路OO號..."
                         hide-details="auto"
                     ></v-text-field>
+                    <!-- 使用常用資料 -->
+                    <v-card-subtitle>
+                        <v-switch @click="checkAutoFill" v-model="autoFill" inset label="填入我的個人資料"></v-switch>
+                    </v-card-subtitle>
+                    <!-- 提醒視窗 -->
+                    <v-dialog v-model="dialog" width="600">
+                        <v-card>
+                            <v-card-title class="font-weight-bold">尚未填寫個人資料</v-card-title>
+                            <v-card-text class="font-weight-bold">我們發現您尚未填寫您的個人檔案，是否先進行填寫?</v-card-text>
+                            <!-- 按鈕操作 -->
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn @click="dialog = false" color="error" outlined>先不要</v-btn>
+                                <v-btn color="primary" nuxt :to="{ name: 'user-account' }">去填寫</v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-dialog>
                 <!-- 按鈕與金額顯示 -->
                 <div class="d-flex align-center justify-center">
                     <!-- 送出按鈕 -->
@@ -123,17 +152,34 @@ export default {
             form: {
                 //* 預設為現金付款
                 payment_id: 1,
+                name: '',
+                phone: '',
                 address: ''
             },
+            //* 自動填入資料 
+            autoFill: false,
+            //* 彈出視窗
+            dialog: false,
             //* 表單驗證
             valid: false,
+            //* 表單驗證規則 
             rules: {
+                numbersOnly: value => {
+                    const pattern = /^[\d ]*$/
+                    return pattern.test(value) || '電話號碼只能有數字。'
+                },
+                lettersOnly: value => {
+                    const pattern = /^\D*$/
+                    return pattern.test(value) || '名字不能包含數字。'
+                },
                 required: value => !!value || '此欄位必填',
-            },
+            }
         }
     },
     computed: {
         ...mapGetters({
+            //* 使用者個人資料 
+            user: 'auth/getUserInfo',
             //* 使用者的購物車資料
             userCart: 'cart/getUserCart', 
             cartValidStatus: 'cart/getValidStatus', //? 確認購物車內是否有商品
@@ -158,6 +204,31 @@ export default {
             //* 建立訂單
             createOrder: 'order/createOrder'
         }),
+        //* 確認是否有使用自動填入功能 
+        checkAutoFill() {
+            this.checkIfUserHasFilledProfile()
+            this.setFormInputs()
+        },
+        //* 確認使用者是否有填入個人資料
+        checkIfUserHasFilledProfile() {
+            if(!this.user.name || !this.user.phone || !this.user.address) {
+                this.dialog = true // 跳出提醒視窗
+                this.autoFill = false
+            }
+        },
+        //* 設定輸入欄位的值
+        setFormInputs() {
+            if(this.autoFill) {
+                this.form.name = this.user.name 
+                this.form.phone = this.user.phone
+                this.form.address = this.user.address
+            } else {
+                this.form.name = ''
+                this.form.phone = ''
+                this.form.address = ''
+            }
+        },
+        //* 建立訂單 
         checkAndCreateOrder() {
             if(this.userCart.length && this.cartValidStatus) {
                 this.createOrder(this.form)
@@ -167,39 +238,9 @@ export default {
 }
 </script>
 
-
 <style lang="scss" scoped>
 $border-radius: 50px;
-//* 標籤
-    //! default (暫不使用) 
-// label {
-//     display: inline-flex;
-//     align-items: center;
-//     position: relative;
-//     border-radius: $border-radius;
-//     box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.5);
-//     text-align: center;
-//     transition: all 0.5s ease-in-out;
-//     cursor: pointer;
-//     //? 文字內容
-//     div {
-//         padding: 12px;
-//         letter-spacing: 4px;
-//         font-weight: bold;
-//     }
-//     //? radio 按鈕
-//     input[type="radio"] {
-//         display: none;
-//         //? check 後改變 label 的背景色
-//         &:checked + div {
-//             background: #1E88E5;
-//             color: #FFF;
-//             border-radius: $border-radius;
-
-//         }
-//     }
-// }
-.checkout_warpper {
+.checkout_wrapper {
     width: 500px;
 }
 .text_field {
