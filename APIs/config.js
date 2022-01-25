8c89ec3f80a477f53ee95d59594a956a5e49f535
AndyHo <h880702520@gmail.com>
2022-01-25 17:43:33 +0800

    add admin request in config

--- APIs/config.js
+++ APIs/config.js
@@ -33,6 +33,44 @@ const userRequest = axios.create({
 //* 使用者請求------------------------------------------------------------------End
     
 
+
+//* 管理者請求-------------------------------------------------------------------Start
+const adminRequest = axios.create({
+    baseURL: `${ base }/admin`
+})
+//? 設置攔截器 (interceptors)
+adminRequest.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('Token')
+        if (token) {
+            config.headers.authorization = token
+        }
+        return config
+    }, 
+    (error) => Promise.reject(error)
+)
+//* 異常(錯誤)處理 
+adminRequest.interceptors.response.use(
+    (response) => {
+        return response
+    }, 
+    (error) => {
+        if(error && error.response) {
+            switch (error.response.status) {
+                case 401:
+                    //* 重新導向
+                    $nuxt._router.push({ name: 'auth-login' })
+                    break; 
+                default:
+                    console.log("axios 錯誤。");
+            }
+        }
+    }
+)
+//* 管理者請求-------------------------------------------------------------------End
+
+
+
 //* 驗證請求-------------------------------------------------------------------Start
 const authRequest = axios.create({
     baseURL: `${ base }/auth/user`
@@ -70,6 +108,7 @@ authRequest.interceptors.response.use(
 
 export default { 
     userRequest,
+    adminRequest,
     productRequest,
     authRequest
 }
