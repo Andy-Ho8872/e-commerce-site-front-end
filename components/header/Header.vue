d3b70e47d31b8ffb3caf1e58b536a8e0b9c884b5
AndyHo <h880702520@gmail.com>
2022-02-12 01:13:00 +0800

    fix route push

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -48,9 +48,9 @@
                             <span>{{ user.name || user.email }}</span>
                         </nuxt-link>
                     </li>
-                    <!--  -->
+                    <!-- 管理者頁面 -->
                     <li class="admin_dashboard" v-if="user.is_admin">
-                        <nuxt-link :to="{ name: 'admin' }">
+                        <nuxt-link :to="{ name: 'admin-product' }">
                             <v-icon class="icon" dark small>fa-user-shield fa-fw</v-icon>
                             <span>商品管理</span>
                         </nuxt-link>
