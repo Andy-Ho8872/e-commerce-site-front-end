2fd4c37a5c70023d0833dd8e5d5aa21df0131832
AndyHo <h880702520@gmail.com>
2022-02-14 16:47:47 +0800

    Update admin.js

--- APIs/admin.js
+++ APIs/admin.js
@@ -3,6 +3,7 @@ import config from '../APIs/config'
 export const apiAdminGetProducts = () => config.adminRequest.get('/product')
 export const apiAdminGetSingleProduct = (product_id) => config.adminRequest.get(`/product/${product_id}`)
 export const apiAdminUpdateProduct = (product_id, data) => config.adminRequest.patch(`/product/${product_id}/update`, data)
+export const apiAdminStoreProduct = (data) => config.adminRequest.post(`/product/store`, data)
 export const apiAdminAddProductVariation = (product_id, data) => config.adminRequest.post(`/product/${product_id}/variation/create`, data)
 export const apiAdminDeleteProductVariation = (product_id, variation_id) => config.adminRequest.delete(`/product/${product_id}/variation/${variation_id}/delete`)
 export const apiAdminUpdateProductVariationOption = (product_id, variation_id, data) => config.adminRequest.patch(`/product/${product_id}/variation/${variation_id}/update`, data)
