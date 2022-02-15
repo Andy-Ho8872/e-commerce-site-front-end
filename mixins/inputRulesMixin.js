2c350240a207224139f90efbe9d41ba4ef05f584
AndyHo <h880702520@gmail.com>
2022-02-15 18:08:52 +0800

    Create inputRulesMixin.js

---
+++ mixins/inputRulesMixin.js
@@ -0,0 +1,17 @@
+export default {
+    data() {
+        return {
+            //* 驗證規則 
+            rules: {
+                required: value => !!value || '此欄位必填',
+                shouldContainOne: value => value.length > 0 || '請至少選擇一個選項',
+                maxLetterLength: value => value.length <= 4 || '字串長度不可超過 4 ex: 0.005',
+                maxLetterLength2: value => value.length <= 3 || '字串長度不可超過 3 ex: 4.55',
+                maxValue: value => value <= 1 || '請依照範圍輸入: 0 ~ 1 ex: 1.00',
+                minValue: value => value > 0  || '請依照範圍輸入: 0 ~ 1 ex: 0.45',
+                maxValue2: value => value <= 5  || '請依照範圍輸入: 1 ~ 5 ex: 4.5',
+                minValue2: value => value >= 1  || '請依照範圍輸入: 1 ~ 5 ex: 1.1',
+            },
+        }
+    }
+}
