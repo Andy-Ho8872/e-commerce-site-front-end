f50e2af6ed3d6613dd7e19925ef79cc5da095df9
AndyHo <h880702520@gmail.com>
2022-03-02 22:04:39 +0800

    add dialog to delete variation

--- pages/admin/product/_id/edit.vue
+++ pages/admin/product/_id/edit.vue
@@ -44,7 +44,7 @@
                         <template v-if="product.variations">
                             <v-card-text v-for="variation in product.variations" :key="variation.id">
                                 <v-chip color="teal" label outlined>{{ variation.title }}</v-chip> 
-                                <v-btn class="mx-2" color="error" small @click="deleteProductVariation({ product_id: product.id, variation_id: variation.id })">刪除規格</v-btn>
+                                <v-btn class="mx-2" color="error" small @click="setCurrentSelectedVariation({ variation_title: variation.title, product_id: product.id, variation_id: variation.id })">刪除規格</v-btn>
                                 <!-- 產品選項 -->
                                 <v-card-text v-for="(option, index) in variation.options" :key="`option_${index}`" class="d-flex align-center">
                                     <span>#{{index + 1}}</span> 
@@ -52,13 +52,33 @@
                                         <div class="chip_wrapper">
                                             <v-chip color="primary" label outlined small class="mx-2">{{ option }}</v-chip>
                                         </div>
+                                        <!-- 刪除選項按鈕 -->
                                         <template v-if="index != 0">
                                             <v-card-text>
-                                                <v-btn color="primary" small @click="deleteProductVariationOption({ product_id: product.id, variation_id: variation.id, options: variation.options, optionIndex: index })">刪除選項</v-btn>
+                                                <v-btn color="error" icon small @click="deleteProductVariationOption({ product_id: product.id, variation_id: variation.id, options: variation.options, optionIndex: index })">
+                                                    <v-icon>fa-trash-alt</v-icon>
+                                                </v-btn>
                                             </v-card-text>
                                         </template> 
                                     </div>
                                 </v-card-text>
+                                <!-- 刪除規格彈出視窗 -->
+                                <v-dialog v-model="deleteVariationDialog" max-width="600">
+                                    <v-card>
+                                        <v-card-title class="font-weight-bold">刪除規格 - {{ currentSelectedVariation.variation_title }}</v-card-title>
+                                        <v-card-text>您即將刪除這個規格，是否繼續?</v-card-text>
+                                        <v-card-actions>
+                                            <v-spacer></v-spacer>
+                                            <v-btn color="error" text @click="deleteVariationDialog = false">取消</v-btn>
+                                            <v-btn color="primary" @click="[deleteProductVariation({ 
+                                                    product_id: currentSelectedVariation.product_id,
+                                                    variation_id: currentSelectedVariation.variation_id 
+                                                }), deleteVariationDialog = false]">
+                                                確認
+                                            </v-btn>
+                                        </v-card-actions>
+                                    </v-card>
+                                </v-dialog>
                             </v-card-text>
                             <!-- 新增規格按鈕 -->
                             <v-card-text>
@@ -66,9 +86,9 @@
                             </v-card-text>
                             <!-- 規格彈出視窗 -->
                             <v-dialog v-model="variationFormDialog" max-width="600" scrollable>
-                                <v-form v-model="dialogValid">
+                                <v-form v-model="variationFormDialogValid">
                                     <v-card class="overflow-auto">
-                                        <v-card-title class="font-weight-bold">新增規格</v-card-title>
+                                        <v-card-title class="font-weight-bold mb-4">新增規格</v-card-title>
                                         <!-- 滾動式內容 -->
                                         <v-card-text style="height: 450px">
                                             <v-card-subtitle>
@@ -76,7 +96,7 @@
                                             </v-card-subtitle>
                                             <v-card-subtitle v-for="(option, index) in variationPayload.variation_options" :key="`variation_option_${index}`">
                                                 <div class="d-flex align-center">
-                                                    <v-text-field ref="variation_option_input" :label="`選項${index + 1}`" placeholder="XL..." name="variation_options[]" v-model="variationPayload.variation_options[index]" :rules="[rules.required]"></v-text-field>
+                                                    <v-text-field ref="variation_option_input" :label="`選項${index + 1}`" placeholder="XL..." name="variation_options[]" @keydown.enter="createVariationOptionInputField" v-model="variationPayload.variation_options[index]" :rules="[rules.required]"></v-text-field>
                                                     <!-- index 不為 0 才顯示 -->
                                                     <template v-if="index != 0">
                                                         <v-btn class="mx-2" color="error" icon small @click="deleteVariationOptionInputField(index)">
@@ -94,7 +114,7 @@
                                         <v-card-actions>
                                             <v-spacer></v-spacer>
                                             <v-btn text color="error" @click="variationFormDialog = false">取消</v-btn>
-                                            <v-btn color="primary" :disabled="!dialogValid"  @click="[addProductVariation(variationPayload), resetVariationPayload()]">確認</v-btn>
+                                            <v-btn color="primary" :disabled="!variationFormDialogValid"  @click="[addProductVariation(variationPayload), resetVariationPayload()]">確認</v-btn>
                                         </v-card-actions>
                                     </v-card>
                                 </v-form>
@@ -128,10 +148,12 @@ export default {
     mixins: [inputRulesMixin], //* 表單驗證規則
     data() {
         return {
+            //* 彈出視窗 
             variationFormDialog: false,
+            deleteVariationDialog: false,
             //* 表單是否通過驗證 
             valid: false,
-            dialogValid: false,
+            variationFormDialogValid: false,
             //* 規格參數 
             variationPayload: {
                 product_id: this.$route.params.id,
@@ -150,6 +172,12 @@ export default {
                     value: '0' 
                 },
             ],
+            //* 當前要刪除的規格
+            currentSelectedVariation: {
+                variation_title: '',
+                product_id: null, 
+                variation_id: null, 
+            },
             //* 表單內容
             formInput: {
                 title: '',
@@ -209,6 +237,14 @@ export default {
         },
         deleteVariationOptionInputField(index) {
             this.variationPayload.variation_options.splice(index, 1)
+        },
+        setCurrentSelectedVariation({ variation_title, product_id, variation_id }) {
+            this.deleteVariationDialog = true
+            this.currentSelectedVariation = {
+                variation_title,
+                product_id, 
+                variation_id, 
+            }
         }
     },
     async mounted() {
